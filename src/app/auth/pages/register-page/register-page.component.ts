c3910644df5f0746918400c113cc3cd019372212
jhernandez-2020477 <jhernandez-2020477@kinal.edu.gt>
2025-11-20 19:47:39 -0600

    Add new validations

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
@@ -16,13 +16,18 @@ export class RegisterPageComponent {
   formUltis = FormUtils
 
   myForm: FormGroup = this.fb.group({
-    name: ['', Validators.required],
-    email: ['', [Validators.required, Validators.email]],
-    username: ['', [Validators.required, Validators.minLength(6)]],
+    name: ['', [Validators.required, Validators.pattern(FormUtils.namePattern)]],
+    email: ['', [Validators.required, Validators.pattern(FormUtils.emailPattern)], [FormUtils.checkingServeResponse]],
+    username: ['', [Validators.required, Validators.minLength(6), Validators.pattern(FormUtils.notOnlySpacesPattern)]],
     password: ['', [Validators.required, Validators.minLength(6)]],
-    password2: ['', Validators.required],
+    password2: ['', [Validators.required]],
+  }, {
+    validators: [
+      FormUtils.isFieldOneEqualFieldTwo('password', 'password2')
+    ]
+  });
 
-  })
+
 
   onSubmit() {
     this.myForm.markAllAsTouched()
